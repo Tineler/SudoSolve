29b5c80b4bab7928e5dab4288d4a158da8d42d4f
Leonard Butz <lbutz@controlthebit.ch>
2014-10-15 19:09:57 +0200

    Added some orientation lines to the sudoku ui

--- src/ch/sudosolve/view/MainWindow.java
+++ src/ch/sudosolve/view/MainWindow.java
@@ -1,28 +1,20 @@
 package ch.sudosolve.view;
 
+import java.awt.Color;
 import java.awt.EventQueue;
 
-import javax.swing.JFrame;
-
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.RowSpec;
-import com.jgoodies.forms.factories.FormFactory;
-
 import javax.swing.JButton;
-
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.GridLayout;
-
-import javax.swing.JLabel;
+import javax.swing.JFrame;
 import javax.swing.JProgressBar;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
+import javax.swing.JSeparator;
+import javax.swing.SwingConstants;
 
 public class MainWindow {
 
 	private JFrame frmSudosolveThe;
+	private JSpinner[][] boardVisualization;
 
 	/**
 	 * Launch the application.
@@ -44,13 +36,14 @@ public class MainWindow {
 	 * Create the application.
 	 */
 	public MainWindow() {
-		initialize();
+		boardVisualization = new JSpinner[9][9];
+		initializeUI();
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	private void initialize() {
+	private void initializeUI() {
 		frmSudosolveThe = new JFrame();
 		frmSudosolveThe.setTitle("SudoSolve - The backtracking Sudoku Solver");
 		frmSudosolveThe.setBounds(100, 100, 450, 500);
@@ -68,13 +61,36 @@ public class MainWindow {
 		JProgressBar progressBar = new JProgressBar();
 		progressBar.setBounds(6, 452, 438, 20);
 		frmSudosolveThe.getContentPane().add(progressBar);
+		
+		JSeparator separator = new JSeparator();
+		separator.setForeground(Color.BLACK);
+		separator.setBounds(6, 92, 365, 12);
+		frmSudosolveThe.getContentPane().add(separator);
+		
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setForeground(Color.BLACK);
+		separator_1.setBounds(6, 185, 365, 12);
+		frmSudosolveThe.getContentPane().add(separator_1);
+		
+		JSeparator separator_2 = new JSeparator();
+		separator_2.setOrientation(SwingConstants.VERTICAL);
+		separator_2.setForeground(Color.BLACK);
+		separator_2.setBounds(122, 6, 12, 276);
+		frmSudosolveThe.getContentPane().add(separator_2);
+		
+		JSeparator separator_3 = new JSeparator();
+		separator_3.setOrientation(SwingConstants.VERTICAL);
+		separator_3.setForeground(Color.BLACK);
+		separator_3.setBounds(245, 6, 12, 276);
+		frmSudosolveThe.getContentPane().add(separator_3);
 
-		SpinnerNumberModel spinnerNumberModel = new SpinnerNumberModel(1, 1, 9, 1);
-		for (int i = 0; i < 9; i++) {
-			for (int k = 0; k < 9; k++) {
+		for (int row = 0; row < 9; row++) {
+			for (int column = 0; column < 9; column++) {
+				SpinnerNumberModel spinnerNumberModel = new SpinnerNumberModel(1, 1, 9, 1);
 				JSpinner spinner = new JSpinner(spinnerNumberModel);
-				spinner.setBounds(6 + 40 * k, 6 + 30 * i, 37, 28);
+				spinner.setBounds(6 + 41 * column, 6 + 31 * row, 37, 28);
 				frmSudosolveThe.getContentPane().add(spinner);
+				boardVisualization[row][column] = spinner;
 			}
 		}
 	}
